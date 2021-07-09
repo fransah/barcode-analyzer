b903b1eacea9d32fa9ba1da1050cc404c9b8b79f
Frans Wessels <frans.wessels@ah.nl>
2021-07-09 12:57:10 +0200

    initial share

---
+++ src/test/kotlin/nl/ah/barcodeanalyzer/BarcodeanalyzerApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.ah.barcodeanalyzer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BarcodeanalyzerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
